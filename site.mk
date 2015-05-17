4a7742fe5070461404419e13c5abc1fe96dcaeec
MrMM <mm@freifunk-aachen.de>
2015-05-17 16:19:58 +0200

    update default experimental release

--- site.mk
+++ site.mk
@@ -37,7 +37,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 2014.4+0-exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 2015.1~0-exp$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
