1d47e75a5ff8590c686115435cf210aca0381289
Malte Moeller <mm@freifunk-aachen.de>
2016-11-08 21:58:34 +0100

    v2016.2.1 is out

--- site.mk
+++ site.mk
@@ -150,9 +150,9 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 2016.2~x-0-exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 2016.2.1-1-exp$(shell date '+%Y%m%d')
 
-DEFAULT_GLUON_CHECKOUT := v2016.2.x
+DEFAULT_GLUON_CHECKOUT := v2016.2.1
 
 ##	GLUON_RELEASE
 #		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
