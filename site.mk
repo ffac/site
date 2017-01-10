3831f9f5fab2d5ca4c8104317f4217a7b944ce8d
Malte Moeller <mm@freifunk-aachen.de>
2017-01-10 11:32:35 +0100

    remove packeges depending von pci for pi hardware

--- site.mk
+++ site.mk
@@ -103,12 +103,7 @@ GLUON_SITE_PACKAGES += \
         kmod-usb-hid \
         kmod-usb-net \
         kmod-usb-net-asix \
-        kmod-usb-net-dm9601-ether \
-        kmod-sky2 \
-        kmod-r8169 \
-        kmod-forcedeth \
-        kmod-8139too \
-        kmod-atl2
+        kmod-usb-net-dm9601-ether
 endif
 
 ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
@@ -119,12 +114,7 @@ GLUON_SITE_PACKAGES += \
         kmod-usb-hid \
         kmod-usb-net \
         kmod-usb-net-asix \
-        kmod-usb-net-dm9601-ether \
-        kmod-sky2 \
-        kmod-r8169 \
-        kmod-forcedeth \
-        kmod-8139too \
-        kmod-atl2
+        kmod-usb-net-dm9601-ether
 endif
 
 ifeq ($(GLUON_TARGET),sunxi)
