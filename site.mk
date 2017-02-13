a5d7773c5fd19c455638d2a91a9588902855e1c5
Malte Moeller <mm@freifunk-aachen.de>
2017-02-13 10:29:55 +0100

    adapt defaults to Gluon 2016.2.3

--- site.mk
+++ site.mk
@@ -140,9 +140,9 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 2016.2.2-3~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 2016.2.3-1~exp$(shell date '+%Y%m%d')
 
-DEFAULT_GLUON_CHECKOUT := v2016.2.2
+DEFAULT_GLUON_CHECKOUT := v2016.2.3
 
 ##	GLUON_RELEASE
 #		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
