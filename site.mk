fc0b953fae71ef89e740173fd1e239924a5687a5
Malte <malte@ffac-build.skydisc.net>
2017-04-09 01:27:07 +0200

    passendes default release zum lede preview

--- site.mk
+++ site.mk
@@ -140,9 +140,9 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 2016.2.4-1~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 2017~1~pre$(shell date '+%Y%m%d')
 
-DEFAULT_GLUON_CHECKOUT := v2016.2.4
+DEFAULT_GLUON_CHECKOUT := master
 
 ##	GLUON_RELEASE
 #		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
