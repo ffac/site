a134a9430c4bbd24ec557d430a0091dee9a98a4c
Malte <malte@ffac-build.skydisc.net>
2017-04-08 20:26:09 +0200

    gluon-next-node wurde ersetzt durch gluon-client-bridge sowie anpassungen an gluon-mesh-batman-adv-14

--- site.mk
+++ site.mk
@@ -21,7 +21,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
 	gluon-luci-wifi-config \
-	gluon-next-node \
+	gluon-client-bridge \
 	gluon-neighbour-info \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
