b9512b9bc82e9fa2a20f275092d4d7e1b5d6ed52
Malte Moeller <mm@freifunk-aachen.de>
2018-06-17 21:01:11 +0200

    remove respondd-module-airtime from default and add to wifi devices only

--- site.mk
+++ site.mk
@@ -26,63 +26,73 @@ GLUON_SITE_PACKAGES := \
 	gluon-radvd \
 	gluon-setup-mode \
 	gluon-status-page \
-	respondd-module-airtime \
 	iwinfo \
 	haveged
 
 # add offline ssid only if the target has wifi device
 ifeq ($(GLUON_TARGET),ar71xx-generic)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
 	gluon-ssid-changer 
 endif
 
 ifeq ($(GLUON_TARGET),ar71xx-mikrotik)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
 	gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),ar71xx-nand)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
 	gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),ar71xx-tiny)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
 	gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),ipq806x)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
         gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),mpc85xx-generic)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
 	gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),mvebu)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
         gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),ramips-mt7620)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
         gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),ramips-mt7628)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
         gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),ramips-rt305x)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
 	gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),ramips-mt7621)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
 	gluon-ssid-changer
 endif
 
@@ -142,6 +152,7 @@ endif
 
 ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
         gluon-ssid-changer \
         kmod-usb-core \
         kmod-usb2 \
@@ -153,6 +164,7 @@ endif
 
 ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
         gluon-ssid-changer \
         kmod-usb-core \
         kmod-usb2 \
@@ -164,6 +176,7 @@ endif
 
 ifeq ($(GLUON_TARGET),brcm2708-bcm2710)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
         gluon-ssid-changer \
         kmod-usb-core \
         kmod-usb2 \
@@ -175,6 +188,7 @@ endif
 
 ifeq ($(GLUON_TARGET),sunxi)
 GLUON_SITE_PACKAGES += \
+	respondd-module-airtime \
         gluon-ssid-changer \
         kmod-usb-core \
         kmod-usb2 \
