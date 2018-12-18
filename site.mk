2fae48526b7cf038828dd903e483fde6ec7dd82b
Malte Moeller <mm@freifunk-aachen.de>
2018-12-18 19:03:24 +0100

    default release is v2018.1.3

--- site.mk
+++ site.mk
@@ -191,9 +191,9 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 2018.1.1-1~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 2018.1.3-1~exp$(shell date '+%Y%m%d')
 
-DEFAULT_GLUON_CHECKOUT := v2018.1.1
+DEFAULT_GLUON_CHECKOUT := v2018.1.3
 
 ##	GLUON_RELEASE
 #		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
