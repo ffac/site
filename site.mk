74a99297690144321b62ef8534197478edd151c8
Malte Moeller <mm@freifunk-aachen.de>
2020-02-16 22:35:25 +0100

    default checkout v2020.1

--- site.mk
+++ site.mk
@@ -193,9 +193,9 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 2019.1.2-1~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 2020.1-1~exp$(shell date '+%Y%m%d')
 
-DEFAULT_GLUON_CHECKOUT := v2019.1.2
+DEFAULT_GLUON_CHECKOUT := v2020.1
 
 ##	GLUON_RELEASE
 #		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
