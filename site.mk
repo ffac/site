3f0f0922322f63f28b773193d49890bfb43912cb
Florian Maurer <f.maurer@outlook.de>
2023-05-20 21:06:19 +0200

    fix site_packages_tiny

--- site.mk
+++ site.mk
@@ -36,8 +36,10 @@ GLUON_SITE_PACKAGES := \
 # gluon-mesh-wireless-sae
 # wireless-encryption-wpa3
 GLUON_FEATURES_standard := \
-    ffac-autoupdater-wifi-fallback \
-    web-cellular \
+    web-cellular
+
+GLUON_SITE_PACKAGES_tiny := \
+    ffac-autoupdater-wifi-fallback
 
 DEFAULT_GLUON_RELEASE := 2023.1.0~exp$(shell date '+%Y%m%d%H')
 
