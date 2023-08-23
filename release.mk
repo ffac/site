bed128a38ce80c7366373d14c551ffccd8681af2
Felix Baumann <felix.bau@gmx.de>
2023-08-23 05:03:46 +0000

    Update to Gluon master (OpenWrt 23)
    make update-patches

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
-GLUON_GIT_URL := https://github.com/herbetom/gluon.git
-GLUON_GIT_REF := 0f0fc05fbcd7012256eb6e9ffd06aaa0540bdeb7 #next-23.05 #v2023.2.x
+GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
+GLUON_GIT_REF := master # v2023.2
