7835b4ea9cc545204ca44a4f367a074ec974e239
FFAC <technik@freifunk-aachen.de>
2024-01-09 23:12:25 +0000

    covr-x1860 is officially supported

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 06d547710bcd1d0e99ed6acb54002f9a46ed23de # master
+GLUON_GIT_REF := 1ba17d6fe2f5bc0f327d42f1627522b709408b67 # master
