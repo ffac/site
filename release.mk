6b238213e32ec95829d28db68cbe56c1ebc16675
Florian Maurer <f.maurer@outlook.de>
2024-01-18 01:02:45 +0100

    update to latest gluon: fixes WDR3600 and WDR4300 issue

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 1ba17d6fe2f5bc0f327d42f1627522b709408b67 # master
+GLUON_GIT_REF := bfbefa4580d1162b2766c6e685d85b970d0c9680 # master
