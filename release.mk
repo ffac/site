38efb9c72a5725027bd676a582179d0ff27d0d80
Florian Maurer <f.maurer@outlook.de>
2024-06-11 14:28:08 +0300

    update gluon and community packages

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 00160f5bd48261156cef1c4e626e1b8dffba2650 # v2023.2.x
+GLUON_GIT_REF := 26118d79ed51d4699da0e0282d467b8fbdf6c40a # v2023.2.x
