ab27b08c26f8ad2c111c62c7e062052167593608
Florian Maurer <f.maurer@outlook.de>
2024-08-09 21:46:28 +0200

    update to latest gluon

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/ffac/gluon.git
-GLUON_GIT_REF := a3f78997bcc6a2318f6d2bbe2115dcc79dff01a6 # next
+GLUON_GIT_REF := 508eda77cbf0146f7069ad584be646e3553f3020 # next
