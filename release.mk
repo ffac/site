93b391c13912123ac0d4f72b80599e6c8919ceb6
FFAC <technik@freifunk-aachen.de>
2024-09-04 16:50:08 +0000

    found broken dap-x1860 airtime usage metric commit in mt76

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := bd5c28e1a9ad58008911e0ec12728737cc01244c # v2023.2.x
+GLUON_GIT_REF := f4736922db46a9e13d40d440f93a0ae1d315dd9f #v2023.2.x
