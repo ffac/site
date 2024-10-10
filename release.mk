f609970810d58b316640005816dd0a6be3aaada5
Florian Maurer <f.maurer@outlook.de>
2024-10-11 00:29:27 +0200

    update to openwrt master, 7530 readd dsl, enable kirkwood, update fb7430

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := bd5c28e1a9ad58008911e0ec12728737cc01244c # v2023.2.x
+GLUON_GIT_REF := 50f05c262d79ef071ada84466467f11e7dd165ff # next
