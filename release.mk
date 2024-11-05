4c6152b9dc00d97ab9331154e8850304f89b9181
Florian Maurer <f.maurer@outlook.de>
2024-11-05 01:31:41 +0100

    use latest next based on openwrt 24.10 branch
    - drop unapplicable patch for fb7430 and mt7621

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 50f05c262d79ef071ada84466467f11e7dd165ff # next
+GLUON_GIT_REF := dc59c71f304fbe6a6a55d662754cee637e4e36d9 # next
