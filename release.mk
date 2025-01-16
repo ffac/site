7af94dd8e5b7e7a4598a87968964a164c0721b39
FFAC <technik@freifunk-aachen.de>
2025-01-16 14:20:45 +0000

    update gluon to working 5GHz mesh with basic_rate patch

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 24a5fd4dc1f9131aa7e8d2dac54ddc75ec8c1b1d # main
+GLUON_GIT_REF := ab1c311845ba72484c88294fe2735fbc028b9ba5 # main
