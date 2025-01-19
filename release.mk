ac35c7f54fd02d65423c8790a9be0c13b8d6a3db
Florian Maurer <f.maurer@outlook.de>
2025-01-19 21:13:00 +0100

    update patches and openwrt base
    fb7530-dsl patch is upstreamed

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := ab1c311845ba72484c88294fe2735fbc028b9ba5 # main
+GLUON_GIT_REF := 3c04d32dd309d75c75a97ae96ccbb0d712b4a938 #main
