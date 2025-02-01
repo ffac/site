971b604b750a7efd868b20d0a1c821dc91a19408
Florian Maurer <f.maurer@outlook.de>
2025-02-01 08:38:39 +0000

    add mt7915-recovery-patch, add mesh1 rssi led, update gluon
    update patches, remove upstreamed patches

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := ad321f50e6c2568d7702cdd5b3253b09c0535198 # main
+GLUON_GIT_REF := cd074cc93d2fdaacbf91c90ca25c6a28f4d1b575 # main
