349c31ef3f4d8617ce4edb8788b320d7a3144f97
FFAC <technik@freifunk-aachen.de>
2025-05-29 08:02:34 +0000

    update gluon/openwrt base

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 5dc5301a846a7a93c292a5e4f908cfe64e0c704e # main
+GLUON_GIT_REF := 73865b6d35626d01c6056c31bf028ebc0aacd071 # main
