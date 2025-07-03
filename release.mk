cd546abc641a832724a4c02cb6a3b4088cadad2e
FFAC <technik@freifunk-aachen.de>
2025-07-03 09:48:12 +0000

    update hop penalty patch

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := c8b1dad318367a73c6df2f46fd92bf7d20e8db7e #main
+GLUON_GIT_REF := 1ad9a3a95d28661140527e28d2f53a1d9383263a # main
