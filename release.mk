7910fe005559d3741b71f11db67b6187554b0a26
Florian Maurer <fmaurer@disroot.org>
2025-07-27 00:16:53 +0000

    update gluon

--- release.mk
+++ release.mk
@@ -1,2 +1,3 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 391988d878ecc94f2625b853f90ec8e62dd810ce # main
+GLUON_GIT_REF := 3b8dff367dcc56f4264e86c346568414d56e66b9 # main
+
