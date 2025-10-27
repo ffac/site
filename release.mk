b0dcc1673336c5d9d458dda4b120cd04d912aca3
FFAC <technik@freifunk-aachen.de>
2025-10-27 12:59:17 +0000

    update to upstream triple radio and updated 6ghz

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := fcabea4f04fe34e3e32f7bf123921b7508d55e98 # main
+GLUON_GIT_REF := 8f28f72546eccf4b49c51fbea000976304e89342 # main
