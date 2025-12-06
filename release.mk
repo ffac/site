26a4b38bde360b3e305eb530460c0d7983d05191
Florian Maurer <f.maurer@outlook.de>
2025-12-06 00:22:07 +0000

    update gluon base

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 8f38662f44f5df69357ced93f166000716f018b3 # main
+GLUON_GIT_REF := 372f5c14f3df42f79b986107c96b3d587b8fb1e0 # main
