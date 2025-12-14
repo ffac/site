f50a9e05974e29a691c1d9cb2220daf017ec7217
FFAC <technik@freifunk-aachen.de>
2025-12-14 13:23:45 +0000

    bump gluon - remove upstreamed ath11k patch

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 7549dcb3b0deef2ae4291a020b27a33c301d4380 # main
+GLUON_GIT_REF := dcd8475bcce51e301cf53ac45dd62115dc7a2b80 # main
