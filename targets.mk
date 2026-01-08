4972b4fa7baa5835c4a482d8fbce28773905c8a4
Florian Maurer <f.maurer@outlook.de>
2026-01-08 02:29:52 +0100

    complete list of targets
    ramips-mt7620 and others are commented to not produce to many unused targets

--- targets.mk
+++ targets.mk
@@ -19,15 +19,21 @@ mpc85xx-p1020
 ramips-mt76x8
 rockchip-armv8
 x86-64
+armsr-armv7
+armsr-armv8
+qualcommax-ipq807x
 endef
+#ramips-mt7620
+#mvebu-cortexa53
+#mvebu-cortexa9
+#lantiq-xrx200_legacy
+#kirkwood-generic
 
 ifneq ($(BROKEN),0)
 define GLUON_TARGETS +=
 bcm27xx-bcm2710
 bcm27xx-bcm2711
 ipq40xx-chromium
-qualcommax-ipq807x
-mvebu-cortexa9
 endef
 endif
 
