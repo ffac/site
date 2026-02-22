4241bf827f1bed2e2ba92d6108292c1c35f31902
Florian Maurer <f.maurer@outlook.de>
2026-02-22 16:39:41 +0000

    update aruba patch and mt76 patch

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 7174d6ef9ceae32fbcfc204c7bc4a12f44d1bbe6 # v2025.1.x
+GLUON_GIT_REF := 5b815ff43135762f5ee5b016765cd0943624694a # v2025.1.x
