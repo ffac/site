56bd75de0c74e3b19bbe9828d6d90538544d9012
FFAC <technik@freifunk-aachen.de>
2026-04-09 11:55:15 +0000

    releae: bump latest v2025.1.x

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 5b815ff43135762f5ee5b016765cd0943624694a # v2025.1.x
+GLUON_GIT_REF := ce838192291c713872bf5cb65faf68803ff8eb80 # v2025.1.x
