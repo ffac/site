a607c52b918855aaca560873d6618a0c0c24e5c1
Florian Maurer <fmaurer@disroot.org>
2026-05-05 02:02:50 +0200

    update  to openwrt-25.12
    update aruba-ap325 patch
    remove upstream 6ghz patch
    ffda-node-whisperer builds fail
    update gluon base with w1700k

--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
-GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := v2025.1.1
+GLUON_GIT_URL := https://github.com/ffac/gluon.git
+GLUON_GIT_REF := 4ab85fbb31ad14699274a88ae6d55d6e1bfc6dc7 # w1700k
