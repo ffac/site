21e47ce5ffac7bc09fd62413c7db1cb0c853b59f
Florian Maurer <fmaurer@disroot.org>
2026-05-05 02:02:50 +0200

    update  to openwrt-25.12
    update aruba-ap325 patch
    remove upstream 6ghz patch
    ffda-node-whisperer builds fail
    update gluon base with w1700k

--- image-customization.lua
+++ image-customization.lua
@@ -70,7 +70,6 @@ if not device_class('tiny') and not tiny_like then
         'ffac-ssid-changer',
         'ff-web-ap-timer',
         'ffda-gluon-usteer',
-        'ffda-node-whisperer',
     }
     -- usb is only default available for non-tiny devices
     include_usb = true
--- release.mk
+++ release.mk
@@ -1,2 +1,2 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := v2025.1.1
+GLUON_GIT_REF := 26f6fb90206970d90b31a5acc539336689e628bc # next
